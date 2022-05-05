d2ce7fa643c7db83e78ac70d829c08fcd917a2ef
Flleeppyy <flleeppyy@gmail.com>
2022-05-05 10:40:32 -0700

    fix: patch functions running twice

--- src/ui/preload.js
+++ src/ui/preload.js
@@ -1,37 +1,86 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+const running = {
+  patcher: {},
+  downloader: {},
+  git: {},
+};
 const SpinHat = {
   patcher: {
     patch: async () => {
-      return await ipcRenderer.invoke("patch");
+      // return await ipcRenderer.invoke("patch");
+      if (!running.patcher.patch) {
+        running.patcher.patch = true;
+        return await ipcRenderer.invoke("patch").finally(() => (running.patcher.patch = false));
+      }
+      return -1931;
     },
     unpatch: async () => {
-      return await ipcRenderer.invoke("unpatch");
+      // return await ipcRenderer.invoke("unpatch");
+      if (!running.patcher.unpatch) {
+        running.patcher.unpatch = true;
+        return await ipcRenderer.invoke("unpatch").finally(() => (running.patcher.unpatch = false));
+      }
+      return -1931;
     },
     isPatched: async () => {
-      return await ipcRenderer.invoke("isPatched");
+      // return await ipcRenderer.invoke("isPatched");
+      if (!running.patcher.isPatched) {
+        running.patcher.isPatched = true;
+        return await ipcRenderer.invoke("isPatched").finally(() => (running.patcher.isPatched = false));
+      }
     },
   },
   downloader: {
     download: async (...args) => {
-      return await ipcRenderer.invoke("download", ...args);
+      // return await ipcRenderer.invoke("download", ...args);
+      if (!running.downloader.download) {
+        running.downloader.download = true;
+        return await ipcRenderer.invoke("download", ...args).finally(() => (running.downloader.download = false));
+      }
+      return -1931;
     },
     update: async (...args) => {
-      return await ipcRenderer.invoke("update", ...args);
+      // return await ipcRenderer.invoke("update", ...args);
+      if (!running.downloader.update) {
+        running.downloader.update = true;
+        return await ipcRenderer.invoke("update", ...args).finally(() => (running.downloader.update = false));
+      }
+      return -1931;
     },
     uninstall: async (...args) => {
-      return await ipcRenderer.invoke("uninstall", ...args);
+      // return await ipcRenderer.invoke("uninstall", ...args);
+      if (!running.downloader.uninstall) {
+        running.downloader.uninstall = true;
+        return await ipcRenderer.invoke("uninstall", ...args).finally(() => (running.downloader.uninstall = false));
+      }
+      return -1931;
     },
   },
   git: {
     getLocalCommit: async () => {
-      return await ipcRenderer.invoke("getLocalCommit");
+      // return await ipcRenderer.invoke("getLocalCommit");
+      if (!running.git.getLocalCommit) {
+        running.git.getLocalCommit = true;
+        return await ipcRenderer.invoke("getLocalCommit").finally(() => (running.git.getLocalCommit = false));
+      }
+      return -1931;
     },
     getRemoteCommit: async () => {
-      return await ipcRenderer.invoke("getRemoteCommit");
+      // return await ipcRenderer.invoke("getRemoteCommit");
+      if (!running.git.getRemoteCommit) {
+        running.git.getRemoteCommit = true;
+        return await ipcRenderer.invoke("getRemoteCommit").finally(() => (running.git.getRemoteCommit = false));
+      }
+      return -1931;
     },
     getChanges: async () => {
-      return await ipcRenderer.invoke("getChanges");
+      // return await ipcRenderer.invoke("getChanges");
+      if (!running.git.getChanges) {
+        running.git.getChanges = true;
+        return await ipcRenderer.invoke("getChanges").finally(() => (running.git.getChanges = false));
+      }
+      return -1931;
     },
   },
   window: {
