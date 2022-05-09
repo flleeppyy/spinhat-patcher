3a3483c3ee4da81e75afb77f9dff83eb1ac9ab5e
Flleeppyy <flleeppyy@gmail.com>
2022-05-09 13:05:59 -0700

    fix: add browser check for settings

--- src/settings.js
+++ src/settings.js
@@ -1,25 +1,55 @@
-const { localStorage } = require("electron-browser-storage");
+// const { localStorage } = require("electron-browser-storage");
+
+let storage;
+// check if we're in a render process
+console.log(process.browser)
+if (process.type === "renderer") {
+  storage = localStorage;
+  console.log(storage);
+} else {
+  storage = require("electron-browser-storage").localStorage;
+  console.log(storage);
+}
+  
+// @ts-check
+const path = require("path");
 
 async function setSetting(setting, value) {
-  const settings = await localStorage.getItem("settings");
+  const settings = await storage.getItem("settings");
   if (settings) {
     const parsed = JSON.parse(settings);
     parsed[setting] = value;
-    await localStorage.setItem("settings", JSON.stringify(parsed));
+    await storage.setItem("settings", JSON.stringify(parsed));
   } else {
-    await localStorage.setItem("settings", JSON.stringify({ [setting]: value }));
+    await storage.setItem("settings", JSON.stringify({ [setting]: value }));
   }
 }
 
 async function getSetting(setting, defaultValue) {
-  const settings = await localStorage.getItem("settings");
+  const settings = await storage.getItem("settings");
   if (settings) {
     const parsed = JSON.parse(settings);
     return parsed[setting] || defaultValue;
   }
 }
 
+/**
+ * 
+ * @returns {Promise<string>}
+ */
+async function getSpinhatPath() {
+  const item = (await getSetting("patchPath"))?.trim();
+  if (item && item?.length > 0) {
+    console.log(item)
+    return item;
+  }
+  const defaultpath = path.join(process.env.USERPROFILE, "AppData", "Roaming", "spinhat")
+  console.log(defaultpath);
+  return defaultpath;
+}
+
 module.exports = {
   setSetting,
   getSetting,
+  getSpinhatPath
 }
